6052456afb307aa2ca84d4eaa53f21903c985190
曾志 <783656285@qq.com>
2019-08-15 09:32:59 +0800

    实现通信

--- js/popup.js
+++ js/popup.js
@@ -4,14 +4,27 @@
  * @Author: Aniu
  * @Date: 2019-08-14 18:24:23
  * @LastEditors: Aniu
- * @LastEditTime: 2019-08-14 18:30:34
+ * @LastEditTime: 2019-08-15 08:48:13
  */
+function sendMessageToContentScript(message, callback) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+      if (callback) callback(response);
+    });
+  });
+}
 $(() => {
   $('.radio-button').on('click', () => {
     var ckb = $('.radio-button__inset');
     if (ckb.is(':checked')) {
-      ckb.attr('checked', 'checked');
+      ckb.attr('checked', false);
       console.log(0);
+      sendMessageToContentScript(
+        { cmd: 'test', value: '你好，我是popup！' },
+        function(response) {
+          console.log('来自content的回复：' + response);
+        }
+      );
     } else {
       ckb.attr('checked', true);
       console.log(1);
